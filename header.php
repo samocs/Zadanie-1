3958294e1ef9bbb7e1b0433fdafdf24fd6edd056
samocs <samoforgi@gmail.com>
2026-03-29 23:13:19 +0200

    header

---
+++ header.php
@@ -0,0 +1,32 @@
+
+<header id="#top">
+
+    <nav class="main-navigation navbar navbar-expand-lg navbar-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.php"><img src="assets/images/white-logo.png" alt=""></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="index.php">Homepage</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="explore.php">Explore Work</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="trending.php">Trending</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+</header>
